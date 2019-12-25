9b47664b69b49349d6b161228d1b0e853f791343
Kui <kui_eugenio@hotmail.com>
2019-12-25 15:18:10 -0700

    attendance component done.
    reads and writes to firestore

--- src/components/Dashboard/Attendance.js
+++ src/components/Dashboard/Attendance.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase, { dbAttendance } from '../Firebase';
 import { connect} from 'react-redux';
-import { Accordion, Form, Card, Button, Row, Col, ListGroup, InputGroup, Badge } from 'react-bootstrap';
+import { Accordion, Form, Card, Button, Row, Col, ListGroup, InputGroup, Badge, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -20,7 +20,8 @@ class Attendance extends Component {
       rotation:0,
       paddlersWhoPracticed:[],
       noLoginPaddler:'',
-      daysThatHadPractices:''
+      daysThatHadPractices:'',
+      showModal:false
     }
   }
   static getDerivedStateFromProps(props, state) {
@@ -49,7 +50,7 @@ class Attendance extends Component {
   }
   getPaddlersWhoAttendedFromDb = () => {
     //convert date to show just the MMDDYYYY instead of timecode
-    const date = moment(this.state.date).format('MMDDYYYY');
+    const date = moment(this.state.date).format('MM-DD-YYYY');
     
     // on the initial mount of this component, get info from db and
     // load up dbPaddlersWhoPracticed and paddlersWhoPracticed to 
@@ -120,29 +121,23 @@ class Attendance extends Component {
     // write to db then update the display for paddlers who practiced
     dbAttendance.doc(moment(this.state.date).format('MM-DD-YYYY')).set({paddler:[...this.state.paddlersWhoPracticed]})
     .then(()=>{
-      this.setState({dBpaddlersWhoPracticed: [...this.state.paddlersWhoPracticed]})
+      this.setState({dBpaddlersWhoPracticed: [...this.state.paddlersWhoPracticed], showModal:false})
     })
     .catch((error)=>{
       console.log("error writing to db:", error);
-      
     })
+  }  
+  closeModal = () =>{
+    this.setState({showModal:false})
   }
-  
-  render() {
-    const highlightWithRanges = [
-      {
-        "react-datepicker__day--highlighted": [
-          new Date('12-12-2019'),
-          new Date('12-14-2019'),
-          new Date('12-16-2019'),
-          new Date('12-18-2019')
-        ]
-      }
-    ];    
+  showModal = () => {
+    this.setState({showModal:true})
+  }
+  render() {    
     return (
       <div className="attendance">
         <Accordion defaultActiveKey="0">      
-        <Card className="text-dark">
+        <Card className="text-dark" bg={'primary'}>
           <Accordion.Toggle as={Card.Title} eventKey="0" className="bg-warning" onClick={this.rotate}>
             <Card.Title className="d-flex justify-content-between align-items-center bg-warning text-dark">
               <span>Attendance</span>
@@ -153,96 +148,111 @@ class Attendance extends Component {
           </Accordion.Toggle>          
           <Accordion.Collapse eventKey='0'>
             <Card.Body>
-              <Card bg="warning" text="dark" >
-                <Card.Title className="align-items-center p-2">
-                <Row>
-                  <Col lg={6} xs={12}>
-                    <Card bg="light" text="white">
-                      <Card.Body className="d-flex justify-content-center">
-                        <DatePicker
-                          value={this.state.date}
-                          selected={this.state.date}
-                          onChange={this.handleCalendarChange}
-                          defaultValue={new Date()}
-                          highlightDates={this.state.daysThatHadPractices}
-                          inline
-                        />                    
-                      </Card.Body>
-                    </Card>              
-                  </Col>
-                  <Col lg={6} xs={12} className="d-flex justify-content-center">
-                    <Card border="light" text="white" className="ml-3 bg-transparent">
-                      <Card.Header className="d-flex justify-content-start">
-                        <span className="ml-2">{moment(this.state.date).format("ddd MM-DD-YYYY")}</span>
-                        <span className="ml-auto">
-                          <Badge pill variant="warning" className="ml-3">{this.state.dBpaddlersWhoPracticed.length} attended</Badge>
-                        </span>
-                      </Card.Header>
+              <Row>
+                <Col lg={6} xs={12}>
+                  <Card bg="light" text="white">
+                    <Card.Body className="d-flex justify-content-center">
+                      <DatePicker
+                        value={this.state.date}
+                        selected={this.state.date}
+                        onChange={this.handleCalendarChange}
+                        defaultValue={new Date()}
+                        highlightDates={this.state.daysThatHadPractices}
+                        inline
+                      />                    
+                    </Card.Body>
+                  </Card>              
+                </Col>
+                <Col lg={6} xs={12} className="d-flex justify-content-center">
+                  <Card border="light" text="white" className="ml-3 bg-transparent dbListOfPaddlersWhoAttended">
+                    <Card.Header className="d-flex justify-content-start">
+                      <span className="ml-2">{moment(this.state.date).format("ddd MM-DD-YYYY")}</span>
+                      <span className="ml-auto">
+                        <Badge pill variant="warning" className="ml-3">{this.state.dBpaddlersWhoPracticed.length} attended</Badge>
+                        <Button className="bg-transparent border-0 ml-2" onClick={this.showModal}><FontAwesomeIcon icon="edit" className="fa-2x"/></Button>
+                      </span>
+                    </Card.Header>
 
-                      <Card.Body>
-                        <Card.Text>
-                          {this.state.dBpaddlersWhoPracticed.map((paddler, i)=>(
-                              <span key={paddler.uid} className="ml-3 comma">{paddler.name}</span>
-                            ))}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                </Row>
-                </Card.Title>
-                <Card.Body>
-                  <Row>
-                    <Col lg={4} xs={12}>
-                        <div className="text-muted paddlersWhoPaddledText">                            
-                          <ListGroup className="listboxText border border-secondary" >
-                            <ListGroup.Item className="bg-dark text-white d-flex justify-content-between">
-                              <div>Paddlers attended:</div>
-                              {( this.state.date ) && (this.state.paddlersWhoPracticed.length > 0) && (<div><Button variant="danger" onClick={this.savePaddlersWhoAttended}><span className="mr-2">Save  </span><FontAwesomeIcon icon="save" className="text-white bg-dark "/></Button></div>)}
-                            </ListGroup.Item>
-                            {this.state.paddlersWhoPracticed.map((paddler, i)=>(
-                              <ListGroup.Item key={paddler.uid||i} onClick={()=>{this.removePaddlerFromPractice(paddler.uid)}} className={paddler.uid.includes('XXXX') && ("text-muted")}>{paddler.name}</ListGroup.Item>
-                            ))}
-                          </ListGroup>
-                        </div>
-                      </Col>
-                    <Col lg={'auto'} xs={12}>  
-                      <Form.Group>
-                        <ListGroup className="listboxText border border-secondary">
-                          <ListGroup.Item className="bg-secondary text-dark">Available Paddlers</ListGroup.Item>
-                          { this.state.paddlers.map( paddler => (
-                            <ListGroup.Item onClick={()=>{this.addPaddlerToPractice(paddler.uid)}} key={paddler.uid}>
-                            {paddler.name}
-                            </ListGroup.Item>
-                          )) }
-                        </ListGroup>
-                      </Form.Group>
-                    </Col>
-                    <Col lg={"auto"} xs={12}>   
-                      <Form onSubmit={this.addNoLoginPaddler} id="addNoLoginPaddler">                                         
-                        <Form.Group controlId="exampleForm.ControlTextarea1" className="p-3 border border-secondary noLogin d-flex justify-content-center flex-column">
-                          <Form.Label>Paddler who does not have a login</Form.Label>
-                          <Form.Row >
-                            <InputGroup onSubmit={()=>{alert('holla')}}>                            
-                              <Form.Control
-                                name="noLoginPaddler"
-                                placeholder="1 paddler per entry"
-                                onChange={this.updateNoLoginPaddler}
-                                value={this.state.noLoginPaddler}
-                              />
-                              <button type="submit">submit</button>
-                            </InputGroup>                                                    
-                          </Form.Row>
-                        </Form.Group>     
-                      </Form>                  
-                    </Col>
-
-                  </Row>
-                </Card.Body>
-              </Card>                      
+                    <Card.Body>
+                      <Card.Text>
+                        {this.state.dBpaddlersWhoPracticed.map((paddler, i)=>(
+                            <span key={paddler.uid} className="ml-3 comma">{paddler.name}</span>
+                          ))}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>                                    
             </Card.Body>
             </Accordion.Collapse>          
           </Card>
         </Accordion>
+        <Modal
+          show={this.state.showModal} onHide={this.closeModal}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          className="attendanceModal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Add Paddlers who attended practice on {moment(this.state.date).format("ddd MMM DD, YYYY")}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Card bg="warning" text="dark" >
+              <Card.Body>
+                <Row>
+                  <Col lg={4} xs={12}>
+                      <div className="text-muted paddlersWhoPaddledText">                            
+                        <ListGroup className="listboxText border border-secondary" >
+                          <ListGroup.Item className="bg-dark text-white d-flex justify-content-between">
+                            <div>Paddlers attended:</div>
+                            {( this.state.date ) && (this.state.paddlersWhoPracticed.length > 0) && (<div><Button variant="danger" onClick={this.savePaddlersWhoAttended}><span className="mr-2">Save  </span><FontAwesomeIcon icon="save" className="text-white bg-dark "/></Button></div>)}
+                          </ListGroup.Item>
+                          {this.state.paddlersWhoPracticed.map((paddler, i)=>(
+                            <ListGroup.Item key={paddler.uid||i} onClick={()=>{this.removePaddlerFromPractice(paddler.uid)}} className={paddler.uid.includes('XXXX') && ("text-muted")}>{paddler.name}</ListGroup.Item>
+                          ))}
+                        </ListGroup>
+                      </div>
+                    </Col>
+                  <Col lg={'auto'} xs={12}>  
+                    <Form.Group>
+                      <ListGroup className="listboxText border border-secondary">
+                        <ListGroup.Item className="bg-secondary text-dark">Available Paddlers</ListGroup.Item>
+                        { this.state.paddlers.map( paddler => (
+                          <ListGroup.Item onClick={()=>{this.addPaddlerToPractice(paddler.uid)}} key={paddler.uid}>
+                          {paddler.name}
+                          </ListGroup.Item>
+                        )) }
+                      </ListGroup>
+                    </Form.Group>
+                  </Col>
+                  <Col lg={"auto"} xs={12}>   
+                    <Form onSubmit={this.addNoLoginPaddler} id="addNoLoginPaddler">                                         
+                      <Form.Group controlId="exampleForm.ControlTextarea1" className="p-3 border border-secondary noLogin d-flex justify-content-center flex-column">
+                        <Form.Label>Paddler who does not have a login</Form.Label>
+                        <Form.Row >
+                          <InputGroup onSubmit={()=>{alert('holla')}}>                            
+                            <Form.Control
+                              name="noLoginPaddler"
+                              placeholder="1 paddler per entry"
+                              onChange={this.updateNoLoginPaddler}
+                              value={this.state.noLoginPaddler}
+                            />
+                            <button type="submit">submit</button>
+                          </InputGroup>                                                    
+                        </Form.Row>
+                      </Form.Group>     
+                    </Form>                  
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeModal}>Close</Button>
+          </Modal.Footer>
+        </Modal>        
       </div>
     );
   }
