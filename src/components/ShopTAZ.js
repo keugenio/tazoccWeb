6e6d622a7ea2cea514f7c6a5b957c488a741034b
Kui <kui_eugenio@hotmail.com>
2020-02-01 21:34:28 -0700

    shopTaz responsive

--- src/components/ShopTAZ.js
+++ src/components/ShopTAZ.js
@@ -5,10 +5,12 @@ import bgImage from '../bgImages/bg_sunset.jpg';
 const ShopTAZ = () => {
   return(
     <React.Fragment> 
-      <div className="bgOverlayColor">
+      <div className="bgOverlayColor storePage">
         <img src={bgImage} className="fullsize-bg-image"></img>
         <div><p className="text-center text-white pageTitle">ShopTAZ</p></div>
-        <Iframe url="store.html" className="store"/>
+        <div className="storeContainer">
+          <Iframe url="store.html" className="store"/>
+        </div>
       </div>
     </React.Fragment>
   )
